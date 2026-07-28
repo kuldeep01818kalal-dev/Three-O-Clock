75f9164a328efd529c6419eb2dd4ae8c848b42ac
kuldeep <kuldeep01818kalal@gmail.com>
2026-07-28 20:35:58 +0530

    Add Style Css Part 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$page_title = "Home";
+
+require_once 'includes/header.php';
+require_once 'includes/navbar.php';
+?>
+
+<div class="container py-5">
+
+    <h1 class="text-center">Welcome to Three O' Clock Cafe</h1>
+
+    <p class="text-center">
+        Website is working successfully.
+    </p>
+
+</div>
+
+<?php
+require_once 'includes/footer.php';
