8ed63a6bf1c6a970332ce0244799bf5e871b47d8
kuldeep <kuldeep01818kalal@gmail.com>
2026-08-05 19:41:55 +0530

    change a-header file and and layout css

--- admin/includes/a-header.php
+++ admin/includes/a-header.php
@@ -1,9 +1,10 @@
 <?php
-if(session_status()===PHP_SESSION_NONE){
+if(session_status() === PHP_SESSION_NONE){
     session_start();
 }
-?>
 
+$currentPage = basename($_SERVER['PHP_SELF']);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,38 +15,32 @@ if(session_status()===PHP_SESSION_NONE){
 <meta name="viewport"
       content="width=device-width, initial-scale=1.0">
 
-<title><?= $pageTitle ?? 'Admin Panel'; ?></title>
+<title><?= $pageTitle ?? 'Three O\' Clock Cafe'; ?></title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" rel="stylesheet">
 
-<link rel="stylesheet" href="../assets/css/admin.css">
-<link rel="stylesheet" href="../assets/css/admin/a-dashboard.css">
-<?php
+<link rel="stylesheet" href="../assets/css/admin/layout.css">
 
-$currentPage = basename($_SERVER['PHP_SELF']);
+<link rel="stylesheet" href="../assets/css/admin/sidebar.css">
 
-if(
-    in_array($currentPage,[
-        'a-dashboard.php',
-        'customer_details.php',
-        'kitchen.php',
-        'orders.php',
-        'view_order.php',
-        'edit_order.php',
-        'a-billing.php',
-        'a-footer.php',
-        'a-sidebar.php'
-    ])
-){
+<link rel="stylesheet" href="../assets/css/admin/navbar.css">
 
-?>
+<link rel="stylesheet" href="../assets/css/admin/footer.css">
 
-<link rel="stylesheet" href="../assets/css/dashboard.css">
+<link rel="stylesheet" href="../assets/css/admin/dashboard.css">
 
-<?php } ?>
+<link rel="stylesheet" href="../assets/css/admin/responsive.css">
 
 </head>
 
 <body>
+
+<div class="admin-layout">
